0f8bc167214cb92850aa44eb96372c92f3a21c89
ZJUZTJ <441867138@qq.com>
2015-10-04 13:45:16 +0800

    添加修改了一些样式

--- mydata.php
+++ mydata.php
@@ -1,12 +1,10 @@
 
-
-<?php
+  <?php
 	session_start();
 	include_once("head.html");
     include_once( "Menu.php" ); 
 	include_once( "config.php" );
     include_once( "class.php" );
-
 	$userid = $_SESSION['user_id'];
 	$password = $_SESSION['user_password'];
 
@@ -30,37 +28,38 @@
 	
 	$result = mysqli_query($db,$query);
 	$num_results = mysqli_num_rows($result);
-	echo "<p>number of user found: ".$num_results."</p>";
 //显示用户信息
-	for($i = 0; $i< $num_results;$i++){
 		$row = mysqli_fetch_assoc($result);
-		echo "<table><div id= \"table\">";
-		echo "<p><strong>".($i+1)." : ";
-		echo "用户注册id";
-		echo htmlspecialchars(stripslashes($row['user_id']));
-		echo "<br />姓名: </strong>";
-		echo stripslashes($row['user_name']);
-		echo "<strong><br />user_picture: </strong>";
-		echo"<div id=\"localImag\"><img id=\"preview\" src = images/".stripslashes($row['user_picture']).".jpg width=40 height=40 style=\"diplay:none\" /></div>";
-		echo "<strong><br />user_description: </strong>";
-		echo stripslashes($row['user_description']);
-		echo "<strong><br />user_message: </strong>";
-		echo stripslashes($row['user_message']);
-		echo "</div></table>";
+		echo "<div class= \"picture\" align = \"center\">";
+		echo" <img id=\"preview\"  src = images/".stripslashes($row['user_picture']).".jpg width=40 height=40 style=\"diplay:none\" /></div>";
+		echo "<div id=\"table\"><table>个人信息</table></div>";
+		echo "<div id=\"table\" align=\"center\" ><table frame=\"void\" rules=\"rows\" >";
+		
+		echo "<tr><th>用户注册id</th>";
+		echo "<th>".htmlspecialchars(stripslashes($row['user_id']))."</th></tr>";
+		echo "<tr><th>姓名: </th>";
+		echo "<th>".stripslashes($row['user_name'])."</th></tr>";
+		echo "<tr><th><strong>user_description: </strong></th>";
+		echo "<th>".stripslashes($row['user_description'])."</th></tr>";
+		echo "<tr><th><strong>user_message: </strong></th>";
+		echo "<th>".stripslashes($row['user_message'])."</th></tr>";
+		echo "</table></div>";
 		echo "</p>";
 		echo "<br />";
-		//显示用户一共参与了多少个小组
+		//显示用户一共参与了多少个小
+		echo "<div id=\"table\"><table>我的小组</table></div>";
 		for($j = 0; $j<$num_groups;$j++){
 			$g_row = mysqli_fetch_assoc($groups);
-			echo "<strong><br />groups".($j+1).": </strong>";
-			echo "<li><a href=\"groups.php?groupid=".stripslashes($g_row['group_id'])."\"><i class='fa fa-envelope-o fa-medium'></i>groups".($j+1)."</li></a>";
+			echo "<table><div id=\"table\"><strong><br />groups".($j+1).": </strong>";
+			echo "<li><a href=\"groups.php?groupid=".stripslashes($g_row['group_id'])."\"><i class='fa fa-envelope-o fa-medium'></i>groups".($j+1)."</li></a></div></table>";
 			echo "<br />";
 		}
 		//显示用户一共参与了多少个活动
+		echo "<div id=\"table\"><table>我的活动</table></div>";
+		echo "<div id=\"table\"><table>";
 		for($k = 0; $k<$num_activitys;$k++){
 			$a_row = mysqli_fetch_assoc($activitys);
-			echo "<strong><br />activitys".($k+1).": </strong>";
-			echo"";
+			echo "<th>";			
 			echo"<li>
 			<a href=\"ground_detail.php?activity_id=".stripslashes($a_row['activity_id'])."\"><i class='fa fa-envelope-o fa-medium'></i>
 			<div id=\"localImag\"><img id=\"preview\" src = images/".stripslashes($a_row['activity_picture']).".jpg width=40 height=40 style=\"diplay:none\" /></div>
@@ -68,13 +67,12 @@
 				</a>
 			</li>";
 			echo stripslashes($a_row['activity_name']);
-			echo "<br />";
+		echo"</th>";
 		}
-
-	}
-	echo "<a href=\"mydata_change.php?user_id=".$userid."\">修改个人信息</a>";
+	echo "<br /></table></div>";
+	echo "<div id=\"table\"><table><div class=\"btn btn-lg\"><a href=\"mydata_change.php?user_id=".$userid."\">修改个人信息</a></br></div></table></div>";
 	
-	echo "<a href=\"about.php\">关于沐途</a>";
+	echo "<div id=\"table\"><table><div class=\"btn btn-lg\"><a href=\"about.php\">关于沐途</a></div></table></div>";
 	
 	mysqli_free_result($result);
     mysqli_close($db);
--- mydata_change.php
+++ mydata_change.php
@@ -2,8 +2,6 @@
 <html>
 <body>
 
-
-
 </body>
 </html>
 
@@ -23,27 +21,27 @@
 	
 	$result = mysqli_query($db,$query);
 	$row = mysqli_fetch_assoc($result);
-	
+	echo"<div class=container>";
 	echo"<form action = \"change_message.php\" method = \"get\" enctype=\"multipart/form-data\">";
     echo"   <h1 class=\"templatemo-header\">change message:</h1><br />";
     echo"<input type=\"hidden\" name=\"user_id\" value=".$user_id.">";         
-    echo"    <h1 >picture:</h1>";
-    echo"    <div class=\"form-group\"><input name = \"user_picture\" type = \"file\" size = \"40\" /> </div><br />";
-    echo"    name:";
-    echo"    <div class=\"form-group\"><input name = \"user_name\" type = \"text\" size = \"40\" value=".stripslashes($row['user_name'])."/></div> <br />";
-      echo"  description:";
-      echo"  <div class=\"form-group\"><input name = \"user_description\" type = \"text\" size = \"40\" value=".stripslashes($row['user_description'])."/></div> <br />";
-       echo" old_password:";
-       echo" <div class=\"form-group\"><input name = \"old_password\" type = \"text\" size = \"40\"  /></div><br />";
-       echo" new_password:";
-       echo"<div class=\"form-group\"><input name = \"new_password\" type = \"text\" size = \"40\" /></div><br />";
-       echo" new_password_again:";
-       echo"  <div class=\"form-group\"><input name = \"new_password2\" type = \"text\" size = \"40\" /></div> <br />";
-       echo"  message: ";
-       echo" <div class=\"form-group\"><input name = \"user_message\" type = \"text\" size=\"40\" value=".stripslashes($row['user_message'])."/></div> <br />";
-       echo"  <input name = \"submit\" type = \"submit\" value = \"insert\" class = \"btn btn-warning\"/>";
+    echo"    <label for=\"name\">picture:</label>";
+    echo"    <div class=\"form-group\"><input class=\"btn btn-warning\" name = \"user_picture\" type = \"file\" /> </div><br />";
+    echo" <label for=\"name\">   name:</label>";
+    echo"    <div class=\"form-group\"><input class=\"form-control\" name = \"user_name\" type = \"text\" size = \"40\" value=".stripslashes($row['user_name'])."/></div> <br />";
+      echo"<label for=\"name\">  description:</label>";
+      echo"  <div class=\"form-group\"><input class=\"form-control\" name = \"user_description\" type = \"text\" size = \"40\" value=".stripslashes($row['user_description'])."/></div> <br />";
+       echo" <label for=\"name\">old_password:</label>";
+       echo" <div class=\"form-group\"><input class=\"form-control\" name = \"old_password\" type = \"text\" size = \"40\"  /></div><br />";
+       echo"<label for=\"name\"> new_password:</label>";
+       echo"<div class=\"form-group\"><input class=\"form-control\" name = \"new_password\" type = \"text\" size = \"40\" /></div><br />";
+       echo" <label for=\"name\">new_password_again:</label>";
+       echo"  <div class=\"form-group\"><input class=\"form-control\" name = \"new_password2\" type = \"text\" size = \"40\" /></div> <br />";
+       echo" <label for=\"name\"> message: </label>";
+       echo" <div class=\"form-group\"><input class=\"form-control\" name = \"user_message\" type = \"text\" size=\"40\" value=".stripslashes($row['user_message'])."/></div> <br />";
+       echo"  <input name = \"submit\" type = \"submit\" value = \"change\"  class=\"btn btn-lg\"/>";
        echo" </form>";
-	echo"<!--<a href=\"products.html\" class=\"btn btn-warning\">View Products</a>-->";			
+	   echo"</div>";		
 	mysqli_free_result($result);
     mysqli_close($db);
 ?>
